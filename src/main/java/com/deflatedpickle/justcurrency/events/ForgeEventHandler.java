25d0ed7070f7e177fed0717e122c859268262c92
DeflatedPickle <DeflatedPickle@gmail.com>
2017-08-17 11:37:26 +0100

    Items now store their value as NBT

--- src/main/java/com/deflatedpickle/justcurrency/events/ForgeEventHandler.java
+++ src/main/java/com/deflatedpickle/justcurrency/events/ForgeEventHandler.java
@@ -2,17 +2,23 @@ package com.deflatedpickle.justcurrency.events;
 
 import net.minecraft.client.resources.I18n;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class ForgeEventHandler {
     @SubscribeEvent
     public void onItemTooltipEvent(ItemTooltipEvent event){
-        ItemStack item = event.getItemStack();
+        ItemStack itemStack = event.getItemStack();
         Float value = 100.0F;
-        String form = "singular";
+        String form = (value <= 1) ? "singular" : "plural";
         String colour = "§6";
 
         event.getToolTip().add(colour + value + " " + I18n.format(String.format("tooltip.justcurrency.currency.%s", form)));
+        if (itemStack.getTagCompound() == null)
+        {
+            itemStack.setTagCompound(new NBTTagCompound());
+        }
+        itemStack.getTagCompound().setFloat("Value", value);
     }
 }
