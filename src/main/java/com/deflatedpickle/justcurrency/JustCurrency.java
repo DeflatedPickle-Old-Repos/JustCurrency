72b8b1cbb2a68add04e5af7189f41002ea3cd2b2
DeflatedPickle <DeflatedPickle@gmail.com>
2017-08-17 08:52:07 +0100

    Added the reference and main file

---
+++ src/main/java/com/deflatedpickle/justcurrency/JustCurrency.java
@@ -0,0 +1,33 @@
+package com.deflatedpickle.justcurrency;
+
+import com.deflatedpickle.justcurrency.proxy.CommonProxy;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION, acceptedMinecraftVersions = Reference.ACCEPTED_VERSIONS)
+public class JustCurrency {
+    @Instance
+    public static com.deflatedpickle.justcurrency.JustCurrency instance;
+
+    @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
+    public static CommonProxy proxy;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+    }
+
+    @EventHandler
+    public void Init(FMLInitializationEvent event) {
+        proxy.init();
+    }
+
+    @EventHandler
+    public void PostInit(FMLPostInitializationEvent event) {
+    }
+}
