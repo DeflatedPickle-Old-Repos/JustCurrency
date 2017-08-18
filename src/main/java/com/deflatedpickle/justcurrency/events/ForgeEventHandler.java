4d7843e91043b0cd73f36002ecfd84dde9778a35
DeflatedPickle <DeflatedPickle@gmail.com>
2017-08-18 06:18:07 +0100

    Improved tooltips

--- src/main/java/com/deflatedpickle/justcurrency/events/ForgeEventHandler.java
+++ src/main/java/com/deflatedpickle/justcurrency/events/ForgeEventHandler.java
@@ -10,11 +10,26 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class ForgeEventHandler {
     @SubscribeEvent
     public void onItemTooltipEvent(ItemTooltipEvent event) {
+        String toolTip = "";
         Float value = ItemUtil.findMatch(event.getItemStack());
 
         String form = GeneralConfig.configCurrency.usePlural ? (value <= 1) ? "singular" : "plural" : "singular";
+        String currencyName = I18n.format(String.format("tooltip.justcurrency.currency.%s", form));
         TextFormatting colour = TextFormatting.getValueByName(GeneralConfig.configCurrency.currencyColour);
 
-        event.getToolTip().add("§6" + value + " " + I18n.format(String.format("tooltip.justcurrency.currency.%s", form)));
+        toolTip += colour.toString();
+        if (!GeneralConfig.configCurrency.namePlacement) {
+            toolTip += value;
+        } else {
+            toolTip += currencyName;
+        }
+        toolTip += " ";
+        if (!GeneralConfig.configCurrency.namePlacement) {
+            toolTip += currencyName;
+        } else {
+            toolTip += value;
+        }
+
+        event.getToolTip().add(toolTip);
     }
 }
